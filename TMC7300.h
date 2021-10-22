b125429c901fbeef165444eb9e9c910023861154
Jim Herd <jtherd@gmail.com>
2021-10-22 17:35:08 +0100

    Renamed project "Robokid_test"

--- TMC7300.h
+++ TMC7300.h
@@ -25,65 +25,65 @@ void  TMC7300_Init(void);
  #define  register_PWMCONF           0x70
 
 // Register GCONF
-#define PWM_DIRECT       ( 0x00, 0x00000001,  0 ) 
-#define EXTCAP           ( 0x00, 0x00000002,  1 ) 
-#define PAR_MODE         ( 0x00, 0x00000004,  2 ) 
-#define TEST_MODE        ( 0x00, 0x00000080,  7 ) 
+#define PWM_DIRECT        0x00000001
+#define EXTCAP            0x00000002
+#define PAR_MODE          0x00000004
+#define TEST_MODE         0x00000080
 
 // Register  GSTAT
-#define RESET            ( 0x01, 0x00000001,  0 ) 
-#define DRV_ERR          ( 0x01, 0x00000002,  1 ) 
-#define U3V5             ( 0x01, 0x00000004,  2 ) 
+#define RESET            0x00000001
+#define DRV_ERR          0x00000002
+#define U3V5             0x00000004
 
 // Register  IFCNT
-#define IFCNT            ( 0x02, 0x000000FF,  0 ) 
+#define IFCNT            0x000000FF
 
 // Register  SLAVECONF
-#define SLAVECONF        ( 0x03, 0x00000F00,  8 ) 
+#define SLAVECONF        0x00000F00
 
 // Register  IOIN
-#define EN               ( 0x06, 0x00000001,  0 ) 
-#define NSTDBY           ( 0x06, 0x00000002,  1 ) 
-#define AD0              ( 0x06, 0x00000004,  2 ) 
-#define AD1              ( 0x06, 0x00000008,  3 ) 
-#define DIAG             ( 0x06, 0x00000010,  4 ) 
-#define UART_ENABLED     ( 0x06, 0x00000020,  5 ) 
-#define UART_INPUT       ( 0x06, 0x00000040,  6 ) 
-#define MODE_INPUT       ( 0x06, 0x00000080,  7 ) 
-#define A2               ( 0x06, 0x00000100,  8 ) 
-#define A1               ( 0x06, 0x00000200,  9 ) 
-#define COMP_A1A2        ( 0x06, 0x00000400, 10 ) 
-#define COMP_B1B2        ( 0x06, 0x00000800, 11 ) 
+#define EN                0x00000001
+#define NSTDBY            0x00000002
+#define AD0               0x00000004
+#define AD1               0x00000008
+#define DIAG              0x00000010
+#define UART_ENABLED      0x00000020
+#define UART_INPUT        0x00000040
+#define MODE_INPUT        0x00000080
+#define A2                0x00000100
+#define A1                0x00000200
+#define COMP_A1A2         0x00000400 
+#define COMP_B1B2         0x00000800
 
 // Register  CURRENT_LIMIT
-#define MOTORRUN         ( 0x10, 0x00000001,  0 ) 
-#define IRUN             ( 0x10, 0x00001F00,  8 ) 
+#define MOTORRUN         0x00000001
+#define IRUN             0x00001F00
 
 // Register  PWM_AB
-#define PWM_A            ( 0x22, 0x000001FF,  0 ) 
-#define PWM_B            ( 0x22, 0x01FF0000, 16 ) 
-#define PWM_AB           ( 0x22, 0x000001FF,  0 ) 
+#define PWM_A            0x000001FF
+#define PWM_B            0x01FF0000
+#define PWM_AB           0x000001FF
 
 // Register  CHOPCONF
-#define ENABLEDRV        ( 0x6C, 0x00000001,  0 ) 
-#define TBL              ( 0x6C, 0x00018000, 15 ) 
-#define DISS2G           ( 0x6C, 0x40000000, 30 ) 
-#define DISS2VS          ( 0x6C, 0x80000000, 31 ) 
+#define ENABLEDRV        0x00000001
+#define TBL              0x00018000
+#define DISS2G           0x40000000
+#define DISS2VS          0x80000000
 
 // Register  DRV_STATUS
-#define OTPW             ( 0x6F, 0x00000001,  0 )  
-#define OT               ( 0x6F, 0x00000002,  1 )  
-#define S2GA             ( 0x6F, 0x00000004,  2 )  
-#define S2GB             ( 0x6F, 0x00000008,  3 )  
-#define S2VSA            ( 0x6F, 0x00000010,  4 )  
-#define S2VSB            ( 0x6F, 0x00000020,  5 )  
-#define OLA              ( 0x6F, 0x00000040,  6 )  
-#define OLB              ( 0x6F, 0x00000080,  7 )  
-#define T120             ( 0x6F, 0x00000100,  8 )  
-#define T150             ( 0x6F, 0x00000200,  9 )  
+#define OTPW             0x00000001 
+#define OT               0x00000002 
+#define S2GA             0x00000004 
+#define S2GB             0x00000008 
+#define S2VSA            0x00000010
+#define S2VSB            0x00000020
+#define OLA              0x00000040
+#define OLB              0x00000080 
+#define T120             0x00000100
+#define T150             0x00000200
 
 // Register  PWMCONF
-#define PWM_FREQ         ( 0x70, 0x00030000, 16 ) 
-#define FREEWHEEL        ( 0x70, 0x00300000, 20 ) 
+#define PWM_FREQ         0x00030000
+#define FREEWHEEL        0x00300000
 
 #endif
