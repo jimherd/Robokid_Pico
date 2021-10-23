218e88c836e1d10c49da0839c81a4c2b1a8e06e0
Jim Herd <jtherd@gmail.com>
2021-10-23 21:20:53 +0100

    Added code to calculate CRC8 using table look-up

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Conversion of original Robokid system to use RP2040 Pico device
 | Original | New|
 |:--------:|:----------:|
 |NXP 68 | Raspberry Pico RP2040 |
-|7-segment | display |SSD1306 |
+|7-segment | SSD1306 LCD|
 
 ### Tools
 
--- TMC7300.c
+++ TMC7300.c
@@ -3,5 +3,17 @@
 
 #include "TMC7300.h"
 
-void  TMC7300_Init(void){
+
+
+void  TMC7300_Init(void) {
+}
+
+uint8_t TMC7300_CRC8(uint8_t *data, uint32_t bytes) {
+
+uint8_t crc = 0;
+    while(--bytes) {
+        crc = crc_table[crc ^ *data++];
+    }
+    return crc;
+
 }
